473fa867215090b78f449a39c7cd112f7c7017f6
yao <yaoyao031@yeah.net>
2021-07-15 10:01:33 +0800

    集合01

---
+++ src/com/mapjihe/CollectionDemo01.java
@@ -0,0 +1,60 @@
+package src.com.mapjihe;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * 集合  ： 非常重要  有一个接口
+ *
+ * 接口可以继承接口，
+ *
+ * length（）方法值得是没有泛型的类
+ * size（），放很多东西  list.add(person) list.add()
+ * 集合可以转换成数组
+ *
+ */
+
+
+public class CollectionDemo01  {
+
+    public static void main(String[] args) {
+        Collection c1=new ArrayList();
+        //add增加
+        ((ArrayList) c1).add("唐僧--执著的目标 ");
+
+        ((ArrayList) c1).add("吉吉国王--过硬的技术");
+        ((ArrayList) c1).add("猪八戒--老色批，体贴关怀");
+        ((ArrayList) c1).add("臭咸鱼--劳动力");
+        ((ArrayList) c1).add("白龙马--坐骑");
+
+        System.out.println("c1="+c1);
+
+        int size=c1.size();
+        System.out.println("size=" +size);
+
+
+        boolean empty=c1.isEmpty();//是否为空
+        System.out.println(empty);
+
+        //集合的清口
+        c1.clear();
+        System.out.println(c1);
+        boolean empty1=c1.isEmpty();
+        System.out.println(empty1);
+
+    
+
+        //集合的便利
+
+        //one
+        for (int i = 0; i < c1.size(); i++) {
+
+            System.out.println(c1);
+
+        }
+
+    }
+
+
+
+}
